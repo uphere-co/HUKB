72bd334006b8550ff747de8491ed3685f2f80a06
Ian-Woo Kim <ianwookim@gmail.com>
2017-11-25 22:25:57 +0000

    add errors in dep.

--- HUKB-driver/default.nix
+++ HUKB-driver/default.nix
@@ -1,11 +1,11 @@
-{ mkDerivation, base, bytestring, lens, text, HWordNet, HUKB, optparse-applicative, stdenv }:
+{ mkDerivation, base, bytestring, errors, lens, text, HWordNet, HUKB, optparse-applicative, stdenv }:
 mkDerivation {
   pname = "HUKB-driver";
   version = "0.1.0.0";
   src = ./.;
   isLibrary = true;
   isExecutable = true;
-  libraryHaskellDepends = [ base bytestring lens text HWordNet HUKB ];
+  libraryHaskellDepends = [ base bytestring errors lens text HWordNet HUKB ];
   executableHaskellDepends = [ base bytestring optparse-applicative text ];
   license = stdenv.lib.licenses.unfree;
 }
