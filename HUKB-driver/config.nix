84b3b0cdee7396c435f46fda8df06b5aeed21e13
Ian-Woo Kim <ianwookim@gmail.com>
2018-01-29 17:08:57 +0000

    add haskellLib.

--- HUKB-driver/config.nix
+++ HUKB-driver/config.nix
@@ -1,12 +1,13 @@
 { pkgs ? import <nixpkgs> {}
 , uphere-nix-overlay ? <uphere-nix-overlay>
 , ukb
+, haskellLib
 }:
 
 with pkgs;
 
 let
-  hsconfig = import (uphere-nix-overlay + "/nix/haskell-modules/configuration-ghc-8.0.x.nix") { inherit pkgs; };
+  hsconfig = import (uphere-nix-overlay + "/nix/haskell-modules/configuration-ghc-8.0.x.nix") { inherit pkgs haskellLib; };
   haskellPackages1 = haskellPackages.override { overrides = hsconfig; };
   HUKBnix = import ../HUKB-generate/default.nix {
     inherit stdenv;
